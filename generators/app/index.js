0b627e5bd8f1ea43c8bde10cc1cc875e82e4fdc6
Scott Prue <scott@residenetwork.com>
2017-01-23 16:03:52 -0800

    Using yeoman 1.0.1 (major syntax change).

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,20 +1,21 @@
 'use strict'
-const yeoman = require('yeoman-generator')
+const Generator = require('yeoman-generator')
 const chalk = require('chalk')
 const yosay = require('yosay')
 const path = require('path')
 const _ = require('lodash')
 
-module.exports = yeoman.Base.extend({
-  initializing: function () {
+module.exports = class extends Generator {
+  initializing () {
     this.argument('name', { type: String, required: false })
-    this.appName = this.name || path.basename(process.cwd()) || 'esnext-library'
-    this.capitalAppName = _.capitalize(_.camelCase(this.appName))
-    this.appPath = this.env.options.appPath
-    this.version = '0.0.1'
-  },
+    this.data = { }
+    this.data.appName = this.name || path.basename(process.cwd()) || 'esnext-library'
+    this.data.capitalAppName = _.capitalize(_.camelCase(this.data.appName))
+    this.data.appPath = this.env.options.appPath
+    this.data.version = '0.0.1'
+  }
 
-  prompting: function () {
+  prompting () {
     this.log(yosay(
       'Welcome to the ' + chalk.red('kyper-library') + ' generator!'
     ))
@@ -40,13 +41,14 @@ module.exports = yeoman.Base.extend({
       }
     ]
 
-    return this.prompt(prompts).then(function (props) {
+    return this.prompt(prompts).then((props) => {
       this.answers = props
+      this.data = Object.assign({}, this.data, this.answers, { answers: this.answers })
       this.githubUser = this.answers.githubUser
-    }.bind(this))
-  },
+    })
+  }
 
-  writing: function () {
+  writing () {
     let filesList = [
       { src: '_package.json', dest: 'package.json' },
       { src: '_README.md', dest: 'README.md' },
@@ -54,9 +56,7 @@ module.exports = yeoman.Base.extend({
       { src: '_config.json', dest: 'config.json' },
       { src: 'gitignore', dest: '.gitignore' },
       { src: 'npmignore', dest: '.npmignore' },
-      { src: 'webpack-base.config.js' },
-      { src: 'webpack-development.config.js' },
-      { src: 'webpack-production.config.js' },
+      { src: 'webpack.config.js' },
       { src: 'src/_index.js', dest: 'src/index.js' },
       { src: 'bin/**', dest: 'bin' },
       { src: 'test/setup.js', dest: 'test/setup.js' },
@@ -74,19 +74,17 @@ module.exports = yeoman.Base.extend({
       filesList.push({ src: 'codeclimate.yml', dest: '.codeclimate.yml' })
     }
     // Make folder before copying to avoid error
-    this.copyFiles(filesList)
-  },
-
-  install: function () {
-    this.npmInstall()
-  },
-  /**
-   * @param {Array|Object} filesArray
-   */
-  copyFiles: function (filesArray) {
-    if (!filesArray) return
-    filesArray.forEach(file =>
-      this.template(file.src || file, file.dest || file.src || file, this.templateContext)
+    filesList.forEach(file =>
+      this.fs.copyTpl(
+        this.templatePath(file.src || file),
+        this.destinationPath(file.dest || file.src || file),
+        this.data
+      )
     )
   }
-})
+
+  install () {
+    this.npmInstall()
+  }
+
+}
